bfdc1c9508e1e4ca8cf8d25988ef9275bb9851e5
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-22 18:31:51 +0100

    Initial version of the homepage done

--- em_potential_solver/src/components/Homepage.tsx
+++ em_potential_solver/src/components/Homepage.tsx
@@ -4,7 +4,15 @@ function Homepage() {
   return (
     <>
       <div className={styles.wrapper}>
-        <p>Plaintext</p>
+        <div className={styles.title}>
+          <h1>Zadanie obliczeniowe</h1>
+          <h2>z przedmiotu <i>Równania różniczkowe i różnicowe</i></h2>
+        </div>
+        <div className={styles.main}>
+          <p>Strona ta stanowi rozwiązanie zadania obliczeniowego z przedmiotu <i>Równania różniczkowe i różnicowe</i>.</p><br />
+          <p>Za pomocą paska nawigacyjnego na górze strony można dostać się do konkretnych części projektu, takich jak <b>wyprowadzenie zagadnienia wariacyjnego</b> oraz <b>rozwiązanie problemu</b>.</p><br />
+          <p>Realizowany wariant zadania: <b>5. Potencjał elektromagnetyczny</b>, wersja na 50 pkt.</p>
+        </div>
       </div>
     </>
   );
