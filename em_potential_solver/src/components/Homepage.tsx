d8fe54fceed88d91f78bf2866e06a488c76d2ef9
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-22 18:07:42 +0100

    First draft of the homepage, managed to center a div

---
+++ em_potential_solver/src/components/Homepage.tsx
@@ -0,0 +1,13 @@
+import styles from './homepage.module.css'
+
+function Homepage() {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <p>Plaintext</p>
+      </div>
+    </>
+  );
+}
+
+export default Homepage;
