a531f54fb4fbfc6d65d48da7d67108bfedeb929b
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-22 18:45:55 +0100

    Basic routing setup in App.tsx

--- em_potential_solver/src/App.tsx
+++ em_potential_solver/src/App.tsx
@@ -1,13 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <Homepage />
-    </>
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- em_potential_solver/src/components/navbar/Navbar.tsx
+++ em_potential_solver/src/components/navbar/Navbar.tsx
@@ -4,6 +4,8 @@ function Navbar() {
   return (
     <div className={styles.wrapper}>
       <p>Plaintext</p>
+      <p>Plaintext</p>
+      <p>Plaintext</p>
     </div>
   );
 }
