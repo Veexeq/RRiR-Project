58f3352667975ce17259074540d38c37b5026ef4
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-23 18:12:08 +0100

    Resolved problems with links to the same page via moving the logic to functions, rather than handling it via <a> tags (they pollute browser history and do not work well with Reacts policy)

--- em_potential_solver/src/components/theory/Theory.tsx
+++ em_potential_solver/src/components/theory/Theory.tsx
@@ -2,6 +2,17 @@ import { BlockMath, InlineMath } from 'react-katex';
 import styles from './theory.module.css'
 
 function Theory() {
+
+  const scrollToSection = (section: string) => (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    const element = document.getElementById(section);
+
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+    }
+  }
+
   return (
     <div className={styles.wrapper}>
       <br/>
@@ -9,8 +20,8 @@ function Theory() {
       <br />
       <p>W tej sekcji znajduje się kompletny wstęp związany z teorią, który potrzebny jest do rozpoczęcia pracy nad znajdowaniem numerycznego rozwiązania problemu. Można go podzielić na następujące części:</p>
       <ul>
-        <li><a href="#intro">Treść problemu</a></li>
-        <li>Opracowanie warunków brzegowych</li>
+        <li><a href="#" onClick={scrollToSection("intro")}>Treść problemu</a></li>
+        <li><a href="#" onClick={scrollToSection("boundary-conditions")}>Opracowanie warunków brzegowych</a></li>
         <li>Wyprowadzenie sformułowania wariacyjnego</li>
         <li>Dyskretyzacja</li>
         <li>Wyprowadzenie układu równań</li>
@@ -31,8 +42,14 @@ function Theory() {
       <p>Fakt, że funkcja <InlineMath math="\epsilon_r(x)" /> posiada nieciągłości jest istotny z perspektywy MES, należy wziąć go pod uwagę dobierając liczbę elementów (<InlineMath math="n" />) na które będziemy dzielili naszą dziedzinę, która tutaj wynosi <InlineMath math="\Omega=[0,3]" />.</p>
       <br /><br />
 
-      <h3>Opracowanie warunków brzegowych</h3>
+      <h3 id="boundary-conditions">Opracowanie warunków brzegowych</h3>
       <br />
+      <p>W powyższym problemie występują dwa warunki brzegowe, które musimy wziąć pod uwagę. Pierwszy z nich to tzw. <b>warunek Robina</b>. Obsługujemy go poprzez wyliczenie wartości funkcji <InlineMath math="\phi'(0)"/>, gdyż pojawi się ona w sformułowaniu słabym równania:</p>
+      <BlockMath math="\phi'(0)+\phi(0) = 5 \quad \Rightarrow \quad \phi'(0) = 5 - \phi(0)" />
+      <p>Drugim warunkiem jest tzw. <b>warunek Dirichleta</b>. W naszym przypadku występuje on w postaci niezerowej (po prawej stronie równania nie ma zera), co jest problematyczne, ponieważ zbiór funkcji, których wartości na brzegu dziedziny są równe zeru nie generują podprzestrzeni liniowej. My natomiast potrzebujemy uzyskać liniowość ze względu na późniejsze obliczenia na macierzach.</p>
+      <br />
+      <p>Aby to umożliwić, dokonamy <b>przesunięcia</b> (shift'u) tego warunku, w efekcie czego będziemy szukać nowego rozwiązania, które posiada zerowy warunek Dirichleta. Pod koniec odzyskamy nasze faktyczne rozwiązanie odwracając podstawienie.</p>
+      <BlockMath math=""/>
     </div>
   );
 }
