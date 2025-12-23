b78f73187bb9abd7e9da591607d06ee5f3ea6289
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-23 14:41:42 +0100

    Properly configured KaTeX-related tools

--- em_potential_solver/src/components/theory/Theory.tsx
+++ em_potential_solver/src/components/theory/Theory.tsx
@@ -1,10 +1,12 @@
+import { BlockMath, InlineMath } from 'react-katex';
 import styles from './theory.module.css'
 
 function Theory() {
   return (
     <div className={styles.wrapper}>
       <h2>Wyprowadzenie sformułowania wariacyjnego</h2>
-      
+      <InlineMath math="y=y(t)"/>
+      <BlockMath math="\frac{dy}{dt} = -ky"/>
     </div>
   );
 }
--- em_potential_solver/src/main.tsx
+++ em_potential_solver/src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import '@fontsource-variable/bricolage-grotesque/index.css';
 import '@fontsource-variable/inter/index.css';
+
+import 'katex/dist/katex.min.css';
 import './index.css';
+
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
