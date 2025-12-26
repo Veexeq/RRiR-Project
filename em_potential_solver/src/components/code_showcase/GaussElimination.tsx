29cba2149cd4b7e5bc19a0a61a295ab79e69be23
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-26 21:49:57 +0100

    Implemented probably final version of code showcase, injected Gauss elimination there

--- em_potential_solver/src/components/code_showcase/GaussElimination.tsx
+++ em_potential_solver/src/components/code_showcase/GaussElimination.tsx
@@ -1,15 +1,49 @@
+import { InlineMath } from "react-katex";
 import CodeBlock from "../ui/CodeBlock";
-import gaussEliminationSC from './../../algo/gaussElimination?raw'
+import gaussEliminationSC from './../../algo/gaussElimination?raw';
+import styles from './showcasepage.module.css';
+import { useState } from "react";
 
 const LANGUAGE = 'typescript';
 
 function GaussElimination() {
+
+  const [codeVisible, setCodeVisible] = useState(false);
+
+  const toggle = () => {
+    setCodeVisible(prev => !prev);
+  };
+
   return (
     <>
-      <CodeBlock 
-        code={gaussEliminationSC}
-        language={LANGUAGE}
-      />
+      <br />
+      <h2>Algorytm eliminacji Gaussa</h2>
+      <br />
+      <p>
+        Prosty algorytm rozwiązywania równania macierzowego <InlineMath math="
+        \mathbf{A} \mathbf{x} = \mathbf{b}"/> dla dowolnego <InlineMath math="n"/> będącego
+        wymiarem macierzy kwadratowej <InlineMath math="\mathbf{A}"/>.
+      </p>
+      <br />
+      <p>Implementacja: <b>TypeScript</b></p>
+      <p>Złożoność obliczeniowa: <InlineMath math="O(n^3)"/></p>
+      <p>Złożoność pamięciowa: <InlineMath math="O(n^2)"/></p>
+      <br />
+      <button 
+        className={styles.button}
+        type='button'
+        onClick={toggle}
+      >{codeVisible ? 'Ukryj kod' : 'Pokaż kod'}
+      </button>
+      <br />
+      {
+        codeVisible && (
+        <CodeBlock 
+          code={gaussEliminationSC}
+          language={LANGUAGE}
+        />
+        )
+      }
     </>
   );
 }
