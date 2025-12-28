804e5dfbe3fb626f158f9c8462bcf5853da51546
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-28 23:02:29 +0100

    WIP: deriving x to xi transformation

--- em_potential_solver/src/components/theory/chapters/GaussLegendre.tsx
+++ em_potential_solver/src/components/theory/chapters/GaussLegendre.tsx
@@ -40,7 +40,53 @@ function GaussLegendre() {
       <br />
       <h4>Transformacja zmiennych dla elementu wzorcowego</h4>
       <br />
-      
+      <p>Przypomnijmy nasz podział dziedziny na elementy skończone:</p>
+      <BlockMath math="
+        \Omega = \bigcup_{i=1}^{n} \, [x_i, \, x_{i+1}] 
+        \quad gdzie \quad [x_i, \, x_{i+1}] = \left[ \tfrac{i-1}{n}, \, \tfrac{i}{n} \right]
+      "/>
+      <p>
+        Nasz element skończony jest więc zdefiniowany jako przedział <InlineMath math="[x_i, x_{i+1}]"/> o
+        długości <InlineMath math="h"/>. Aby obliczyć całkę na takim elemencie, musimy wpierw dokonać trasformacji
+        współrzędnych do układu odniesienia <InlineMath math="\xi"/>. Zrobimy to za pomocą następującego odwzorowania liniowego:
+      </p>
+      <BlockMath math="
+        x(\xi) = \frac{x_{i+1} + x_i}{2} + \frac{x_{i+1} - x_i}{2} \cdot \xi
+      "/>
+      <p>Sprawdźmy, czy jest ono poprawne:</p>
+      <BlockMath math="
+        x(-1) = \frac{x_{i+1} + x_i}{2} - \frac{x_{i+1} - x_i}{2} = x_i \\[2ex]
+        x(1) = \frac{x_{i+1} + x_i}{2} + \frac{x_{i+1} - x_i}{2} = x_{i+1}
+      "/>
+      <p>Wszystko się zgadza. Możemy to jeszcze nieco uprościć wyznaczając dwie równoważne formy:</p>
+      <BlockMath math="
+      \begin{array}{c|c}
+        \begin{align*}
+          x(\xi) &= \frac{x_{i+1} + x_i}{2} + \frac{x_{i+1} - x_i}{2} \cdot \xi \\[2ex]
+          &= x_{mid} + \frac{h}{2} \, \xi \\[2ex]
+        \end{align*}
+        \qquad & \qquad
+        \begin{align*}
+          x(\xi) &= \frac{x_{i+1} - x_i + 2x_i}{2} + \frac{x_{i+1} - x_i}{2} \cdot \xi \\[2ex]
+          &= x_i + \frac{x_{i+1} - x_i}{2} \cdot (1 + \xi) \\[2ex]
+          &= x_i + \frac{h}{2} \cdot (1 + \xi) 
+        \end{align*}
+      \end{array}
+      "/>
+      <p>
+        Udało nam się sensownie przekształcić dziedzinę elementu skończonego na dziedzinę całki. Wzór <InlineMath math="x=x(\xi)"/>{' '}
+        pozwala nam przy użyciu punktu kwadratury <InlineMath math="\xi_k"/> odnaleźć się w dziedzinie <InlineMath math="\Omega"/>, co
+        z kolei pozwoli nam m.in. na odzyskanie wartości funkcji <InlineMath math="\epsilon_r(x)"/>.
+      </p>
+      <br />
+      <p>
+        Należy pamiętać o tym, że zmiana granic całkowania pociąga za sobą również konieczność przeskalowania różniczki. Obliczmy Jakobian
+        przekształcenia: 
+      </p>
+      <BlockMath math="
+        J = \frac{dx}{d\xi} = \frac{d}{d\xi} (x_{mid} + \frac{h}{2} \, \xi) = \frac{h}{2} \\[2ex]
+        dx = \frac{h}{2} \, d\xi
+      "/>
     </>
   );
 }
