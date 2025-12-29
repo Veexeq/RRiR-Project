1e349cf1a010521377f90ba9e782df6726b6d401
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-12-30 00:24:56 +0100

    Minor adjustments, added icon and title, changed BrowserRouter in App.tsx to allow running 'npm run dev'

--- em_potential_solver/src/App.tsx
+++ em_potential_solver/src/App.tsx
@@ -10,7 +10,7 @@ import Solver from "./components/solver/Solver";
 function App() {
 
   return (
-    <BrowserRouter basename="/RRiR-Project">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       {/* TopScroller is an util that scrolls to the top of
       the page after changing pages */}
       <TopScroller />
